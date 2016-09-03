a832548ec5f7129d9f2711803ea0f7555f017064
Adarsh Venugopal <adivenu123@gmail.com>
2016-09-04 00:03:57 +0530

    Add files via upload

---
+++ InfiniteLoop.java
@@ -0,0 +1,13 @@
+package infiniteloop;
+
+/**
+ *
+ * @author Adarsh
+ */
+public class InfiniteLoop {
+    public static void main(String...args){
+        while(true){
+        //program just hangs
+        }
+    }
+}
